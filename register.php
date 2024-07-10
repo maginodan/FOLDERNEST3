1913b9fa5a3ae96833bbd673626aea137a5df2d6
TEBUSEKE123 <137684246+TEBUSEKE123@users.noreply.github.com>
2024-07-10 13:36:36 -0700

    register page code

---
+++ register.php
@@ -0,0 +1,137 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    session_start();
+    if (isset($_SESSION['SESSION_EMAIL'])) {
+        header("Location: index.php");
+        die();
+    }
+
+    
+    require 'vendor/autoload.php';
+    include 'connection/config.php';
+    $msg = "";
+
+    if (isset($_POST['submit'])) {
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+        $confirm_password = mysqli_real_escape_string($conn, md5($_POST['confirm-password']));
+        $code = mysqli_real_escape_string($conn, md5(rand()));
+        
+        // Add a new variable to store the user role
+        $role = 'user';
+
+        // Ensure the first registered user is an admin
+        $existing_users = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM users"));
+        if ($existing_users == 0) {
+            $role = 'admin';
+        }
+
+        if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM users WHERE email='{$email}'")) > 0) {
+            $msg = "<div class='alert alert-danger'>{$email} - This email address already exists.</div>";
+        } else {
+            if ($password === $confirm_password) {
+                $sql = "INSERT INTO users (name, email, password, code, role) VALUES ('{$name}', '{$email}', '{$password}', '{$code}', '{$role}')";
+                $result = mysqli_query($conn, $sql);
+
+                if ($result) {
+                    echo "<div style='display: none;'>";
+                    $mail = new PHPMailer(true);
+
+                    try {
+                        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                        $mail->isSMTP();
+                        $mail->Host       = 'smtp.gmail.com';
+                        $mail->SMTPAuth   = true;
+                        $mail->Username   = 'danmagino64@gmail.com';
+                        $mail->Password   = 'xzcy zjns exzn bajh';
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                        $mail->Port       = 465;
+
+                        $mail->setFrom('danmagino64@gmail.com', 'FOLDERNEST.COM');
+                        $mail->addAddress($email);
+
+                        $mail->isHTML(true);
+                        $mail->Subject = 'no reply';
+                        $mail->Body = 'Here is the verification link <b><a href="http://localhost/FOLDERNEST3/login.php?verification='.$code.'">http://localhost/FOLDERNEST3/login.php?verification='.$code.'</a></b>';
+                        
+                        $mail->send();
+                        echo 'Message has been sent';
+                    } catch (Exception $e) {
+                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                    }
+                    echo "</div>";
+                    $msg = "<div class='alert alert-info'>We've sent a verification link on your email address.</div>";
+                } else {
+                    $msg = "<div class='alert alert-danger'>Something went wrong.</div>";
+                }
+            } else {
+                $msg = "<div class='alert alert-danger'>Password and Confirm Password do not match</div>";
+            }
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>FOLDER NEST</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="Login Form" />
+    <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <script src="https://kit.fontawesome.com/af562a2a63.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <section class="w3l-mockup-form">
+        <div class="container">
+            <div class="workinghny-form-grid">
+                <div class="main-mockup">
+                    <div class="alert-close">
+                        <span class="fa fa-close"></span>
+                    </div>
+                    <div class="w3l_form align-self">
+                        <div class="left_grid_info">
+                            <img src="images/gal.png" alt="">
+                        </div>
+                    </div>
+                    <div class="content-wthree">
+                        <div class="logo">
+                            <a href="register.php" class="app-brand-link gap-2">
+                                <img src="images/logo3.png" alt="Your Logo Alt Text" class="app-brand-logo">
+                                <span class="app-brand-text">Folder Nest</span>
+                                <p>"At your service" </p>
+                            </a>
+                        </div>
+                        <p>Register Now</p>
+                        <?php echo $msg; ?>
+                        <form action="" method="post">
+                            <input type="text" class="name" name="name" placeholder="Enter Your Name" value="<?php if (isset($_POST['submit'])) { echo $name; } ?>" required>
+                            <input type="email" class="email" name="email" placeholder="Enter Your Email" value="<?php if (isset($_POST['submit'])) { echo $email; } ?>" required>
+                            <input type="password" class="password" name="password" placeholder="Enter Your Password" required>
+                            <input type="password" class="confirm-password" name="confirm-password" placeholder="Enter Your Confirm Password" required>
+                            <button name="submit" class="btn" type="submit">Register</button>
+                        </form>
+                        <div class="social-icons">
+                            <p>Have an account! <a href="login.php">Login</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="js/jquery.min.js"></script>
+    <script>
+        $(document).ready(function (c) {
+            $('.alert-close').on('click', function (c) {
+                $('.main-mockup').fadeOut('slow', function (c) {
+                    $('.main-mockup').remove();
+                });
+            });
+        });
+    </script>
+</body>
+</html>
